59d778537de14457db236916ff545b33e090b901
Francisco Palomeque <palito10fran@gmail.com.ar>
2021-06-22 15:54:00 -0300

    Ortografia sectores

--- src/components/Sectores.jsx
+++ src/components/Sectores.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../estilocards.css';
-import imgaudit from './assets/imagenes/auditor.png'
+import imgaudit from './assets/imagenes/auditoria2.png'
 import imgproceso from './assets/imagenes/proceso-de-diseno.png'
 import imgcapacitacion from './assets/imagenes/capacitacion.png'
 import imgentrevista from './assets/imagenes/entrevista.png'
@@ -11,34 +11,34 @@ const Sectores = () => {
   <div className="card" id="card1">
     <h1 className="titulo">Diseño de procesos</h1>
     <img src={imgproceso}></img>
-    <p className="descripcion">Elaboracion y Adecuacion de Politicas y Procedimientos para su Implementacion y Cumplimiento Normativo. Manuales. Codigos. Lineamientos</p>
+    <p className="descripcion">Elaboración y Adecuación de Políticas y Procedimientos para su Implementación y Cumplimiento Normativo. Manuales. Códigos. Lineamientos</p>
   </div>
 
   <div className="card" id="card2">
   <h1 className="titulo">REI</h1>
-  <p className="descripcion">Revisor Externo Independiente: Unidad de Informacion Financiera (UIF). Emision y presentacion ante la UIF de informes de revision externa independiente vinculadas al cumplimiento, por su parte de los Sujetos Obligados</p>
+  <p className="descripcion">Revisor Externo Independiente: Unidad de Información Financiera (UIF). Emisión y presentación ante la UIF de informes de revisión externa independiente vinculadas al cumplimiento, por su parte de los Sujetos Obligados</p>
   </div>
 
   <div className="card" id="card3">
   <h1 className="titulo">Capacitacion</h1>
   <img src={imgcapacitacion}></img>
-  <p className="descripcion">Diseño y Elaboracion de Programas de Capacitacion para los distintos niveles de recursos humanos</p>
+  <p className="descripcion">Diseño y Elaboracion de Programas de Capacitación para los distintos niveles de recursos humanos</p>
     </div>
 <div className="card"id="card4">
 
 <h1 className="titulo">Auditoria</h1>
       <img src={imgaudit}></img>
-  <p className="descripcion">Control interno global de identificacion, evaluacion, migracion y monitoreo de Riesgos de LA/FT. Revisiones integrales para la identificacion de deficiencias o mejoras a aplicar</p>
+  <p className="descripcion">Control interno global de identificación, evaluación, migración y monitoreo de Riesgos de LA/FT. Revisiones integrales para la identificación de deficiencias o mejoras a aplicar</p>
     </div>
 <div className="card"id="card5">
 <h1 className="titulo">Comite</h1>
 <img src={imgentrevista}></img>
-  <p className="descripcion">Participacion en Comite de Compliance o PLA&FT Redaccion de Minutas. Seguimiento de temas tratados.</p>
+  <p className="descripcion">Participación en Comité de Compliance o PLA&FT Redacción de Minutas. Seguimiento de temas tratados.</p>
     </div>
 <div className="card"id="card6">
 <h1 className="titulo">Requerimientos</h1>
 <img src={imgarchivo}></img>
-  <p className="descripcion">Asistencia en Reuqerimientos o Procesos de Supervision de Organismos de COntrol/Reguladores</p>
+  <p className="descripcion">Asistencia en Requerimientos o Procesos de Supervisión de Organismos de Control/Reguladores</p>
     </div>
 </div>
     );

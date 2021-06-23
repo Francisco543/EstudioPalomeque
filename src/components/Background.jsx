7335d1b0baf664e55a29fa28db2a828753e1a4f7
Francisco Palomeque <palito10fran@gmail.com.ar>
2021-06-23 18:10:12 -0300

    arreglando video fondo

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -15,12 +15,28 @@ const StyledVideo = styled.video`
     }
 `;
 
+const VideoDiv = styled.div`
+    position: fixed;
+    z-index: -1; 
+    
+    @media (min-aspect-ratio:16/9){
+        width: 100%;
+        height: auto;
+    }
+    @media (max-aspect-ratio:16/9){
+        width: auto;
+        height: 100%;
+    }
+
+`;
+
 const Background = () => {
-    return (         
+    return (   
+        <VideoDiv>    
         <StyledVideo autoPlay  muted >
             <source src={VideoHeader} type="video/mp4"/>
         </StyledVideo>  
-               
+        </VideoDiv>         
      );         
 }
  
